58709fd9096e5176f92c87786d25213ecbeaec84
irving <zhouyongtao@outlook.com>
2015-06-28 16:02:16 +0800

    md  weixin sub

--- src/main/java/com/ellt/web/controller/WeixinController.java
+++ src/main/java/com/ellt/web/controller/WeixinController.java
@@ -1,6 +1,7 @@
 package com.ellt.web.controller;
 import com.alibaba.fastjson.JSON;
 import com.github.sd4324530.fastweixin.api.OauthAPI;
+import com.github.sd4324530.fastweixin.api.UserAPI;
 import com.github.sd4324530.fastweixin.api.config.ApiConfig;
 import com.github.sd4324530.fastweixin.api.enums.OauthScope;
 import com.github.sd4324530.fastweixin.api.response.GetUserInfoResponse;
@@ -9,6 +10,7 @@ import com.github.sd4324530.fastweixin.handle.EventHandle;
 import com.github.sd4324530.fastweixin.handle.MessageHandle;
 import com.github.sd4324530.fastweixin.message.BaseMsg;
 import com.github.sd4324530.fastweixin.message.TextMsg;
+import com.github.sd4324530.fastweixin.message.req.BaseEvent;
 import com.github.sd4324530.fastweixin.message.req.TextReqMsg;
 import com.github.sd4324530.fastweixin.servlet.WeixinControllerSupport;
 import org.joda.time.DateTime;
@@ -17,8 +19,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import static com.alibaba.fastjson.JSON.toJSON;
 import static com.alibaba.fastjson.JSON.toJSONString;
 
 /**
@@ -49,17 +51,35 @@ public class WeixinController extends WeixinControllerSupport {
     protected String getAESKey() {
         return null;
     }
+
     //重写父类方法，处理对应的微信消息
     @Override
     protected BaseMsg handleTextMsg(TextReqMsg msg) {
         String content = msg.getContent();
         logger.debug("用户发送到服务器的内容:{}", content);
-        return new TextMsg(DateTime.now().toString() +" 服务器回复用户消息 by irving !");
+        return new TextMsg("hava fun,by irving !");
     }
+
+    @Override
+    protected BaseMsg handleSubscribe(BaseEvent event) {
+        logger.info("handleSubscribe json: "+ toJSON(event));
+        UserAPI userAPI =new UserAPI(new ApiConfig(AppId,AppSecret));
+        GetUserInfoResponse user= userAPI.getUserInfo(event.getFromUserName());
+        String msg ="Hi "+user.getNickname()+" 欢迎关注流量通!";
+        return new TextMsg(msg);
+    }
+
+    @Override
+    protected BaseMsg handleUnsubscribe(BaseEvent event) {
+        logger.info("handleUnsubscribe json: "+ toJSON(event));
+        return super.handleUnsubscribe(event);
+    }
+
+
     /*1.1版本新增，重写父类方法，加入自定义微信消息处理器
-     *不是必须的，上面的方法是统一处理所有的文本消息，如果业务觉复杂，上面的会显得比较乱
-     *这个机制就是为了应对这种情况，每个MessageHandle就是一个业务，只处理指定的那部分消息
-     */
+         *不是必须的，上面的方法是统一处理所有的文本消息，如果业务觉复杂，上面的会显得比较乱
+         *这个机制就是为了应对这种情况，每个MessageHandle就是一个业务，只处理指定的那部分消息
+         */
     @Override
     protected List<MessageHandle> initMessageHandles() {
         List<MessageHandle> handles = new ArrayList<MessageHandle>();
